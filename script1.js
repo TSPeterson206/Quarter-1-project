f3dc2df200e4b6818ba69cb8089594da8d956f47
TOBY PETERSON <TSPETERSON@)^@GMAIL.COM>
2018-10-16 13:05:58 -0700

    condensed functions into one, renamed functions for clarity

--- script1.js
+++ script1.js
@@ -1,80 +1,41 @@
-//var savedGoal1 = g1text.innerHTML
-
 //goal entry functions
 
 let g1text = document.getElementById('g1par')
-
-function goal1set(){
-    g1text.innerHTML = ''
-    g1text.innerHTML += "<div>Goal: " + document.getElementById('goal1text').value + "</div>"
-    g1text.innerHTML += "<div>Start: " + document.getElementById('goal1Start').value + "</div>"
-    g1text.innerHTML += "<div>Due By: " + document.getElementById('goal1End').value + "</div>"
-    g1text.innerHTML += "<div>Report To: " + document.getElementById('accountability1').value + "</div>"
-    g1text.innerHTML += "<div>Phone: " + document.getElementById('accountability1Phone').value + "</div>"
-    g1text.innerHTML += "<div>Email: " + document.getElementById('accountability1Email').value + "</div>"
-    localStorage.setItem("first", g1text.innerHTML);
-}
-
 let g2text = document.getElementById('g2par')
-
-function goal2set(){
-    g2text.innerHTML = ''
-    g2text.innerHTML += "<div>Goal: " + document.getElementById('goal2text').value + "</div>"
-    g2text.innerHTML += "<div>Start: " + document.getElementById('goal2Start').value + "</div>"
-    g2text.innerHTML += "<div>Due By: " + document.getElementById('goal2End').value + "</div>"
-    g2text.innerHTML += "<div>Report To: " + document.getElementById('accountability2').value + "</div>"
-    g2text.innerHTML += "<div>Phone: " + document.getElementById('accountability2Phone').value + "</div>"
-    g2text.innerHTML += "<div>Email: " + document.getElementById('accountability2Email').value + "</div>"
-    localStorage.setItem("second", g2text.innerHTML);
-}
-
 let g3text = document.getElementById('g3par')
-
-function goal3set(){
-    g3text.innerHTML = ''
-    g3text.innerHTML += "<div>Goal: "+ document.getElementById('goal3text').value + "</div>"
-    g3text.innerHTML += "<div>Start: " + document.getElementById('goal3Start').value + "</div>"
-    g3text.innerHTML += "<div>Due By: " + document.getElementById('goal3End').value + "</div>"
-    g3text.innerHTML += "<div>Report To: " + document.getElementById('accountability3').value + "</div>"
-    g3text.innerHTML += "<div>Phone: " + document.getElementById('accountability3Phone').value + "</div>"
-    g3text.innerHTML += "<div>Email: " + document.getElementById('accountability3Email').value + "</div>"
-    localStorage.setItem("third", g3text.innerHTML);
-}
-
 let g4text = document.getElementById('g4par')
 
-function goal4set(){
-    g4text.innerHTML = ''
-    g4text.innerHTML += "<div>Goal: " + document.getElementById('goal4text').value + "</div>"
-    g4text.innerHTML += "<div>Start: " + document.getElementById('goal4Start').value + "</div>"
-    g4text.innerHTML += "<div>Due By: " + document.getElementById('goal4End').value + "</div>"
-    g4text.innerHTML += "<div>Report To: " + document.getElementById('accountability4').value + "</div>"
-    g4text.innerHTML += "<div>Phone: " + document.getElementById('accountability4Phone').value + "</div>"
-    g4text.innerHTML += "<div>Email: " + document.getElementById('accountability4Email').value + "</div>"
-    localStorage.setItem("fourth", g4text.innerHTML);
+function goalSet(goalBox, goalNumber){
+    goalBox.innerHTML = ''
+    goalBox.innerHTML += "<div>Goal: " + document.getElementById(`goal${goalNumber}text`).value + "</div>";
+    goalBox.innerHTML += "<div>Start: " + document.getElementById(`goal${goalNumber}Start`).value + "</div>";
+    goalBox.innerHTML += "<div>Due By: " + document.getElementById(`goal${goalNumber}End`).value + "</div>";
+    goalBox.innerHTML += "<div>Report To: " + document.getElementById(`accountability${goalNumber}`).value + "</div>";
+    goalBox.innerHTML += "<div>Phone: " + document.getElementById(`accountability${goalNumber}Phone`).value + "</div>";
+    goalBox.innerHTML += "<div>Email: " + document.getElementById(`accountability${goalNumber}Email`).value + "</div>"
+    localStorage.setItem(`${goalNumber}`, goalBox.innerHTML);
 }
-
 
 document.querySelector('#goaladd1 > form').addEventListener('submit', function(event){
 event.preventDefault()
 document.querySelector('#goaladd1').style.display = 'none'
-goal1set() })
+goalSet(g1text, 1) })
 
 
 document.querySelector('#goaladd2 > form').addEventListener('submit', function(event){
 event.preventDefault()
 document.querySelector('#goaladd2').style.display = 'none'
-goal2set() })
+goalSet(g2text, 2) })
         
 document.querySelector('#goaladd3 > form').addEventListener('submit', function(event){
 event.preventDefault()
 document.querySelector('#goaladd3').style.display = 'none'
-goal3set() })
+goalSet(g3text, 3) })
            
 document.querySelector('#goaladd4 > form').addEventListener('submit', function(event){
 event.preventDefault()
 document.querySelector('#goaladd4').style.display = 'none'
-goal4set() })
+goalSet(g4text, 4) })
     
 
 
@@ -82,7 +43,7 @@ goal4set() })
 
 var tickTickBoom = document.getElementById("calendar1");
 
-function newFunction () {
+function tallyDays () {
     var days = Date.now()-new Date(tickTickBoom.value).getTime();
     document.getElementById('timeTicker').textContent = Math.round(days/86400000) + " days";
     var storedDate = Math.round(days/86400000) + " days";
@@ -90,13 +51,13 @@ function newFunction () {
     document.getElementById('timeTicker').textContent = localStorage.getItem("savedDate");
 }
 
-tickTickBoom.addEventListener('change', newFunction)
+tickTickBoom.addEventListener('change', tallyDays)
 
 // sobriety counter functionality - minutes
 
 var tickTickBoomer = document.getElementById("calendar2");
 
-function newerFunction () {
+function tallyMinutes () {
     var minutes = Date.now()-new Date(tickTickBoomer.value).getTime();
     document.getElementById('timeTicker').textContent = Math.round(minutes/60000) + " mins";
     var storederDate = Math.round(minutes/60000) + " mins";
@@ -104,13 +65,13 @@ function newerFunction () {
     document.getElementById('timeTicker').textContent = localStorage.getItem("savederDate");
 }
 
-tickTickBoomer.addEventListener('change', newerFunction)
+tickTickBoomer.addEventListener('change', tallyMinutes)
 
 // sobriety counter functionality - hours
 
 var tickTickBoomest = document.getElementById("calendar3");
 
-function newestFunction () {
+function tallyHours () {
     var hours = Date.now()-new Date(tickTickBoomest.value).getTime();
     document.getElementById('timeTicker').textContent = Math.round(hours/3600000) + " hours";
     var storedestDate = Math.round(hours/3600000) + " hours";
@@ -118,7 +79,7 @@ function newestFunction () {
     document.getElementById('timeTicker').textContent = localStorage.getItem("savedestDate");
 }
 
-tickTickBoomest.addEventListener('change', newestFunction)
+tickTickBoomest.addEventListener('change', tallyHours)
 
 
 
@@ -128,13 +89,13 @@ document.getElementById('timeTicker').textContent = localStorage.getItem("savede
 document.getElementById('timeTicker').textContent = localStorage.getItem("savedestDate");
 
 //local storage for goal1
-g1text.innerHTML = localStorage.getItem("first");
+g1text.innerHTML = localStorage.getItem("1");
 //local storage for goal2
-g2text.innerHTML = localStorage.getItem("second");
+g2text.innerHTML = localStorage.getItem("2");
 //local storage for goal3
-g3text.innerHTML = localStorage.getItem("third");
+g3text.innerHTML = localStorage.getItem("3");
 //local storage for goal4
-g4text.innerHTML = localStorage.getItem("fourth");
+g4text.innerHTML = localStorage.getItem("4");
 
 //Goal 1 progress bar
 var firstProg = document.getElementById('progBar1');
@@ -145,14 +106,14 @@ var thirdProg = document.getElementById('progBar3');
 //Goal 4 progress bar
 var fourthProg = document.getElementById('progBar4');
 
-document.getElementById('goal1button').addEventListener('click', () => daysBetween1(goal1Start.value,goal1End.value))
-document.getElementById('goal2button').addEventListener('click', () => daysBetween2(goal2Start.value,goal2End.value))
-document.getElementById('goal3button').addEventListener('click', () => daysBetween3(goal3Start.value,goal3End.value))
-document.getElementById('goal4button').addEventListener('click', () => daysBetween4(goal4Start.value,goal4End.value))
+document.getElementById('goal1button').addEventListener('click', () => daysBetween1(goal1Start.value,goal1End.value, firstProg))
+document.getElementById('goal2button').addEventListener('click', () => daysBetween1(goal2Start.value,goal2End.value, secondProg))
+document.getElementById('goal3button').addEventListener('click', () => daysBetween1(goal3Start.value,goal3End.value, thirdProg))
+document.getElementById('goal4button').addEventListener('click', () => daysBetween1(goal4Start.value,goal4End.value, fourthProg))
 
 
 
-function daysBetween1 (date1,date2) {
+function daysBetween1 (date1,date2, barNum) {
     
     var newDate1 = date1.toString();
     var newDate2 = date2.toString();
@@ -161,20 +122,20 @@ function daysBetween1 (date1,date2) {
     var diff = date2Time - date1Time;
     var whatsTheDifferenceBetweenMeAndYou = Math.round(diff); 
     var kiddingMe = whatsTheDifferenceBetweenMeAndYou/100;
-    var progWidth1 = firstProg.style.width
-
-    const id = setInterval(bumpUp, 400)
+    var progWidth1 = barNum.style.width
+    //interval should be the var kiddingMe, set to 1000 for exhibition purposes
+    const id = setInterval(bumpUp, 1000)
 
     function bumpUp(){
-        if (firstProg.style.width !== "100%") {
+        if (barNum.style.width !== "100%") {
             
             var newVar = progWidth1.slice(0,length-1)
             var newerVar = parseInt(newVar)
             newerVar+=1
             newestVar = newerVar.toString() + "%"
             progWidth1 = newestVar
-            firstProg.style.width = progWidth1
-            document.getElementById('progBar1').textContent = progWidth1
+            barNum.style.width = progWidth1
+            barNum.textContent = progWidth1
         }
         else {
             clearInterval(id);
@@ -182,101 +143,6 @@ function daysBetween1 (date1,date2) {
         }
     }
 }
-
-
-function daysBetween2 (date1,date2) {
-    
-    var newDate1 = date1.toString();
-    var newDate2 = date2.toString();
-    var date1Time = new Date(newDate1).getTime();
-    var date2Time = new Date(newDate2).getTime();
-    var diff = date2Time - date1Time;
-    var whatsTheDifferenceBetweenMeAndYou = Math.round(diff); 
-    var kiddingMe = whatsTheDifferenceBetweenMeAndYou/100;
-    var progWidth1 = secondProg.style.width
-
-    const id = setInterval(bumpUp, 800)
-
-    function bumpUp(){
-        if (secondProg.style.width !== "100%") {
-            
-            var newVar = progWidth1.slice(0,length-1)
-            var newerVar = parseInt(newVar)
-            newerVar+=1
-            newestVar = newerVar.toString() + "%"
-            progWidth1 = newestVar
-            secondProg.style.width = progWidth1
-            document.getElementById('progBar2').textContent = progWidth1
-        }
-        else {
-            clearInterval(id);
-            alert ("Goal Completed!"); 
-        }
-    }
-}
-
-function daysBetween3 (date1,date2) {
-    
-    var newDate1 = date1.toString();
-    var newDate2 = date2.toString();
-    var date1Time = new Date(newDate1).getTime();
-    var date2Time = new Date(newDate2).getTime();
-    var diff = date2Time - date1Time;
-    var whatsTheDifferenceBetweenMeAndYou = Math.round(diff); 
-    var kiddingMe = whatsTheDifferenceBetweenMeAndYou/100;
-    var progWidth1 = thirdProg.style.width
-
-    const id = setInterval(bumpUp, 3000)
-
-    function bumpUp(){
-        if (thirdProg.style.width !== "100%") {
-            
-            var newVar = progWidth1.slice(0,length-1)
-            var newerVar = parseInt(newVar)
-            newerVar+=1
-            newestVar = newerVar.toString() + "%"
-            progWidth1 = newestVar
-            thirdProg.style.width = progWidth1
-            document.getElementById('progBar3').textContent = progWidth1
-        }
-        else {
-            clearInterval(id);
-            alert ("Goal Completed!"); 
-        }
-    }
-}
-
-function daysBetween4 (date1,date2) {
-    
-    var newDate1 = date1.toString();
-    var newDate2 = date2.toString();
-    var date1Time = new Date(newDate1).getTime();
-    var date2Time = new Date(newDate2).getTime();
-    var diff = date2Time - date1Time;
-    var whatsTheDifferenceBetweenMeAndYou = Math.round(diff); 
-    var kiddingMe = whatsTheDifferenceBetweenMeAndYou/100;
-    var progWidth1 = fourthProg.style.width
-
-    const id = setInterval(bumpUp, 6000)
-
-    function bumpUp(){
-        if (fourthProg.style.width !== "100%") {
-            
-            var newVar = progWidth1.slice(0,length-1)
-            var newerVar = parseInt(newVar)
-            newerVar+=1
-            newestVar = newerVar.toString() + "%"
-            progWidth1 = newestVar
-            fourthProg.style.width = progWidth1
-            document.getElementById('progBar4').textContent = progWidth1
-        }
-        else {
-            clearInterval(id);
-            alert ("Goal Completed!"); 
-        }
-    }
-}
-
 
 //Username/Profile established
 var profile = document.getElementById('profileName')
@@ -292,6 +158,8 @@ logBut.addEventListener('click', changeProfile)
 
 profile.textContent = localStorage.getItem("profileName") 
 
+
+// Clear Buttons
 document.getElementById('goal1butt').addEventListener('click', clear1)
 document.getElementById('goal2butt').addEventListener('click', clear2)
 document.getElementById('goal3butt').addEventListener('click', clear3)
